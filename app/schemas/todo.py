f2e3f933d2d75dd11f5e20fae9d55860a72eb0a8
mosahel01 <mos.sahil01@gmail.com>
2026-07-27 22:02:30 +0530

    feat(schemas): add TodoCreate model and default value for completed

--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -5,9 +5,16 @@ class Todo(BaseModel):
     userId: int
     id: int
     title: str
+    completed: bool = False  # sets a default value if not provided during creation
+
+
+class TodoCreate(BaseModel):
+    userId: int
+    title: str
     completed: bool
 
 
+# No ID as server is responsible for creating IDs
 # pydantic model = schema
 # pydantic validates/filters outgoing data
 # from routes before fastapi converts it to JSON
