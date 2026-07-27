c5741102b4af01a6a34cf3290e3afaccadd0b29b
mosahel01 <mos.sahil01@gmail.com>
2026-07-27 22:03:14 +0530

    feat(services): add create_todo_service and rename fetch_json to fetch_service

--- app/services/todo_service.py
+++ app/services/todo_service.py
@@ -3,7 +3,8 @@ import requests
 from app.config import BASE_URL, REQUEST_TIMEOUT
 
 
-def fetch_json(endpoint: str):
+# GET Service
+def fetch_service(endpoint: str):
     response = requests.get(f"{BASE_URL}{endpoint}", timeout=REQUEST_TIMEOUT)
     response.raise_for_status()  # avoids ignoring HTTP errors
     return response.json()
@@ -11,20 +12,37 @@ def fetch_json(endpoint: str):
 
 def fetch_todos(completed: bool | None = None):
     if completed is None:
-        return fetch_json("/todos")
-    return fetch_json(f"/todos?completed={str(completed).lower()}")
+        return fetch_service("/todos")
+    return fetch_service(f"/todos?completed={str(completed).lower()}")
 
 
 def fetch_todo_by_id(todo_id: int):
-    return fetch_json(f"/todos/{todo_id}")
+    return fetch_service(f"/todos/{todo_id}")
 
 
 def fetch_user_todos(user_id: int):
-    return fetch_json(f"/users/{user_id}/todos")
+    return fetch_service(f"/users/{user_id}/todos")
 
 
 def fetch_user_by_id(user_id: int):
-    return fetch_json(f"/users/{user_id}")
+    return fetch_service(f"/users/{user_id}")
+
+
+# # POST Service
+# def create_service(endpoint: str):
+#     response = requests.post(f"{BASE_URL}/todos", json={endpoint}.model_dump())
+#     return response.json()
+
+# def create_todo_service(todo: TodoCreate):
+#     return create_service(todo)
+
+def create_todo_service(todo: TodoCreate):
+    response = requests.post(
+        f"{BASE_URL}/todos", 
+        json=todo.model_dump()
+    )
+
+    return response.json()
 
 
 # business logic here
--- main.py
+++ main.py
@@ -4,4 +4,3 @@ from app.routes.todos import router
 
 app = FastAPI()  # creates fastapi app
 app.include_router(router)  # includes all routers
-
