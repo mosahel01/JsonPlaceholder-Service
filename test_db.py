d9522c3197e8efb317ffd16a7a533613c5b55139
mosahel01 <mos.sahil01@gmail.com>
2026-07-30 17:34:52 +0530

    test(db): establish PostgreSQL connection validation harness for todo_app

--- test_db.py
+++ test_db.py
@@ -2,7 +2,12 @@ import psycopg
 
 connection = psycopg.connect( # connection represents open convo with DB
     dbname="todo_app",
-    user="postgres",
+    user="todo_app_user",
+    password="todo_password",
     host="localhost",
     port=5432,
 )
+
+# cursor = connection.cursor()
+# cursor.execute(...)
+# fetchall()
