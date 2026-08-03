27c5217f3a16f70958cb06277bbc5eb6523a83ee
mosahel01 <mos.sahil01@gmail.com>
2026-08-03 21:11:30 +0530

    test(db): validate write-path connectivity by inserting a seeded todo record

--- test_db.py
+++ test_db.py
@@ -1,6 +1,7 @@
 import psycopg
 
-connection = psycopg.connect( # connection represents open convo with DB
+# opens a connection with PostgreSQL
+connection = psycopg.connect( 
     dbname="todo_app",
     user="todo_app_user",
     password="todo_password",
@@ -8,6 +9,25 @@ connection = psycopg.connect( # connection represents open convo with DB
     port=5432,
 )
 
-# cursor = connection.cursor()
-# cursor.execute(...)
-# fetchall()
+# create something that can execute SQL
+cursor = connection.cursor() 
+
+# send SQL query to PostgreSQL
+cursor.execute(
+    """
+    INSERT INTO todos(title)
+    VALUES ('Learn Docker');
+    """
+)
+
+connection.commit()
+
+print("Todo Inserted")
+
+connection.close()
+
+# # receives all the rows into python
+# rows = cursor.fetchall()
+#
+# for row in rows:
+#     print(row)
